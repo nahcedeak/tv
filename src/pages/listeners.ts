e9220ce45f90a7ecd4d0678f4ba9dd468ad5e86b
decade <mstarnum@outlook.com>
2021-05-08 16:18:54 +0800

    '🍎'

--- src/pages/listeners.ts
+++ src/pages/listeners.ts
@@ -38,7 +38,7 @@ function isBottom() {
   const docClientHeight = document.documentElement.clientHeight
   const docScrollHeight = document.documentElement.scrollHeight
 
-  setElementText('#channelMessage',docScrollTop + ' ' + docClientHeight + ' ' + docScrollHeight)
+  setElementText('#channelMessage',(docScrollTop + docClientHeight - docScrollHeight).toString())
 
   return docScrollTop + docClientHeight === docScrollHeight ? true : false
 }
